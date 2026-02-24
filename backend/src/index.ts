cd0620914e729e2929648bc416be03c1b5234a11
imnotmilena <stepanyanmil7.ait@gmail.com>
2026-02-24 19:46:36 +0400

    server running startup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello TypeScript');
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from 'path';
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.get("/api/",(req,res)=>{
+    res.json({message: "Hello from Backend"})
+})
+const PORT = 3011 
+app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
